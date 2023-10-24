7e01731fff289ebfcb6e5e0a80c88429491f69d8
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-10-24 13:33:44 +0530

    rbs-shell

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[34mInstalling MongoDB Server\e[0m"
 yum  install mongodb-org -y &>>/tmp/roboshop.log
 
-echo -e "\e[34mUpdate MongoDB Listen Address\e[0m'
+echo -e "\e[34mUpdate MongoDB Listen Address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[34mStart MongoDB Service\e[0m"
