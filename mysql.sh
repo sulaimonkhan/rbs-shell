3919b47dd0da210614e4f7c84f9ca0f3af667e42
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-10-26 13:54:40 +0530

    as

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+echo -e "\e[34m Disable MySQL Default Version \e[0m"
+yum  module disable mysql -y
+
+echo -e "\e[34m Install MySQL Community Server \e[0m"
+yum install mysql-community-server -y
+
+echo -e "\e[34m Start MySQL Service\e[0m"
+systemctl enable mysqld
+systemctl restart mysqld 
+
+echo -e "\e[34m Setup MySQL Password \e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
