1da70499519078c68e56ad1a99e6db153b7eacbd
sulaimonkhan <sulaimonkhan04@gmail.com>
2023-10-26 14:02:34 +0530

    as

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 echo -e "\e[34m Disable MySQL Default Version \e[0m"
 yum  module disable mysql -y &>>/tmp/roboshop.log
 
+echo -e "\e[34m Copy MySQL Repo File \e[0m"
+cp /home/centos/rbs-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+
 echo -e "\e[34m Install MySQL Community Server \e[0m"
 yum install mysql-community-server -y &>>/tmp/roboshop.log
 
